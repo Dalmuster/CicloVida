4973a488588201b36ccc25d28e3d60dc1e7eb5f9
Daniel <dalmusterpereira@danielcastelao.org>
2025-04-10 18:48:36 +0200

    Apartado 6-Prueba a la hora de definir tamaños en composable

--- app/src/main/java/com/example/ciclovida/MainActivity.kt
+++ app/src/main/java/com/example/ciclovida/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.ciclovida.ui.theme.CicloVidaTheme
 
 class MainActivity : ComponentActivity() {
@@ -72,7 +73,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingText(message:String, modifier: Modifier = Modifier) {
     Text(
-        text = message
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp
     )
 }
 
